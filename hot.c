bb2fef29f645b4e0407f144f84c9f2f4eb2d676d
TOJO Rikuto <s1310210@sshsv212.u-aizu.ac.jp>
2024-07-04 17:16:50 +0900

    HoT: initial revision

---
+++ hot.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const char* coin_toss() {
+    return rand() % 2 == 0 ? "Heads" : "Tails";
+}
+
+int main() {
+    srand(time(NULL));
+
+    printf("Tossing a coin...\n");
+    const char* results[3];
+    int hcount = 0;
+    int tcount = 0;
+
+    for (int i = 0; i < 3; i++) {
+        results[i] = coin_toss();
+        if (results[i][0] == 'H') {
+            hcount++;
+        } else {
+            tcount++;
+        }
+        printf("Round %d: %s\n", i + 1, results[i]);
+    }
+
+    printf("Heads: %d, Tails: %d\n", hcount, tcount);
+
+    return 0;
+}j
